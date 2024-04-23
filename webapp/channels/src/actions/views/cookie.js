e6ba61b07228329df23c6ed1bbdbc6953760f56d
j.yao.SUSE <everhopingandwaiting@users.noreply.github.com>
2024-04-23 11:09:29 +0000

    auto login with clean cookie first

--- webapp/channels/src/actions/views/cookie.js
+++ webapp/channels/src/actions/views/cookie.js
@@ -14,4 +14,9 @@ export function clearUserCookie() {
     document.cookie = `MMCSRF=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=${window.basename}`;
     document.cookie = `MMCSRF=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=/`;
     document.cookie = `MMCSRF=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=${window.basename}`;
+
+    document.cookie = 'MMAUTHTOKEN=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
+    document.cookie = `MMAUTHTOKEN=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=${window.basename}`;
+    document.cookie = `MMAUTHTOKEN=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=/`;
+    document.cookie = `MMAUTHTOKEN=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=${window.basename}`;
 }
